66de7e27ac07a89f36467a140dc2aab681ca72cd
João Neto <joaonetoprivado2001@gmail.com>
2023-09-23 07:36:15 -0300

    Delete ufpi/4° Período directory

--- ufpi/4° Período/ESTRUTURAS_DE_DADOS_II/trabalho-referente-a-segunda-avaliacao/arvore-vermelho-preta/questao1/lista.h
+++
@@ -1,6 +0,0 @@
-typedef struct lista Lista;
-
-Lista* iniciaLista();
-Lista* insereLista(Lista *lista, int linha);
-
-void mostrarLinhas(Lista *lista);
--- ufpi/4° Período/files/game-tic-tac-toe_v6/cliente_teste.py
+++
@@ -1,7 +0,0 @@
-import socket
-ip = 'localhost'
-# ip = '10.180.42.110'
-port = 1618
-address = ((ip, port))
-cliente_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-cliente_socket.connect(address)
